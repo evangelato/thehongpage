64e61366b77d56a468a7b33f8156f3330f7fe2d8
Evan Hong <evangelurtz@gmail.com>
2019-12-19 15:55:40 +0900

    Add sidebar menu options

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -10,6 +10,11 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
+import CodeIcon from '@material-ui/icons/Code';
+import BuildIcon from '@material-ui/icons/Build';
+import PaletteIcon from '@material-ui/icons/Palette';
 
 const Sidebar: React.FC = () => {
     const isSidebarOpen = useSelector((state: any) => state.Sidebar.isSidebarOpen);
@@ -25,11 +30,42 @@ const Sidebar: React.FC = () => {
                         </ListItemIcon>
                         <ListItemText>Home</ListItemText>
                     </ListItem>
-                    <ListItem button key="Resume" component={NavLink} to="/resume">
+                    <ListItem button key="AboutMe" component={NavLink} to="/about_me">
                         <ListItemIcon>
                             <InfoIcon />
                         </ListItemIcon>
-                        <ListItemText>Resume</ListItemText>
+                        <ListItemText>About Me</ListItemText>
+                    </ListItem>
+                    <ListItem button key="WorkExperience" component={NavLink} to="/work_experience">
+                        <ListItemIcon>
+                            <WorkIcon />
+                        </ListItemIcon>
+                        <ListItemText>Work Experience</ListItemText>
+                    </ListItem>
+                    <ListItem button key="Education" component={NavLink} to="/education">
+                        <ListItemIcon>
+                            <SchoolIcon />
+                        </ListItemIcon>
+                        <ListItemText>Education</ListItemText>
+                    </ListItem>
+                    <ListItem button key="PersonalProjects" component={NavLink} to="/personal_projects">
+                        <ListItemIcon>
+                            <CodeIcon />
+                        </ListItemIcon>
+                        <ListItemText>Personal Projects</ListItemText>
+                    </ListItem>
+                    <ListItem button key="Skills" component={NavLink} to="/skills">
+                        <ListItemIcon>
+                            <BuildIcon />
+                        </ListItemIcon>
+                        <ListItemText>Skills</ListItemText>
+                    </ListItem>
+                    
+                    <ListItem button key="Hobbies" component={NavLink} to="/hobbies">
+                        <ListItemIcon>
+                            <PaletteIcon />
+                        </ListItemIcon>
+                        <ListItemText>Hobbies</ListItemText>
                     </ListItem>
                 </List>
             </div>
