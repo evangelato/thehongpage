e4f37341f61351933178dbc55e32dacafcf68415
Evan Hong <evangelurtz@gmail.com>
2020-03-06 19:56:04 +0900

    Add transition animation to education page

--- src/containers/Education/index.tsx
+++ src/containers/Education/index.tsx
@@ -10,6 +10,7 @@ import Hidden from '@material-ui/core/Hidden';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Grow from '@material-ui/core/Grow';
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -71,80 +72,90 @@ const Education: React.FC = () => {
                     </div>
                 </Container>
                 <Container maxWidth="lg">
-                    <Paper
-                        className={classes.mainFeaturedPost}
-                        style={{
-                            backgroundImage: `url(${data.header && data.header.imageUrl ? data.header.imageUrl : ''})`,
-                        }}
-                    >
-                        <div className={classes.overlay} />
-                        <Grid container>
-                            <Grid item md={6}>
-                                <div className={classes.mainFeaturedPostContent}>
-                                    <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                                        {data.header && data.header.title ? data.header.title : ''}
-                                    </Typography>
-                                    <Typography variant="h5" color="inherit" paragraph>
-                                        {data.header && data.header.subtitle1 ? data.header.subtitle1 : ''}
-                                    </Typography>
-                                    <Typography variant="h6" color="inherit" paragraph>
-                                        {data.header && data.header.subtitle2 ? data.header.subtitle2 : ''}
-                                    </Typography>
-                                </div>
+                    <Grow in style={{ transformOrigin: '0 0 0' }} timeout={500}>
+                        <Paper
+                            className={classes.mainFeaturedPost}
+                            style={{
+                                backgroundImage: `url(${
+                                    data.header && data.header.imageUrl ? data.header.imageUrl : ''
+                                })`,
+                            }}
+                        >
+                            <div className={classes.overlay} />
+                            <Grid container>
+                                <Grid item md={6}>
+                                    <div className={classes.mainFeaturedPostContent}>
+                                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                                            {data.header && data.header.title ? data.header.title : ''}
+                                        </Typography>
+                                        <Typography variant="h5" color="inherit" paragraph>
+                                            {data.header && data.header.subtitle1 ? data.header.subtitle1 : ''}
+                                        </Typography>
+                                        <Typography variant="h6" color="inherit" paragraph>
+                                            {data.header && data.header.subtitle2 ? data.header.subtitle2 : ''}
+                                        </Typography>
+                                    </div>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </Paper>
+                        </Paper>
+                    </Grow>
                     <Grid container spacing={4}>
-                        <Grid item xs={12} md={6}>
-                            <Card className={classes.card}>
-                                <div className={classes.cardDetails}>
-                                    <CardContent>
-                                        <Typography component="h2" variant="h5">
-                                            {data.academics && data.academics.title ? data.academics.title : ''}
-                                        </Typography>
-                                        <Typography variant="subtitle1" paragraph>
-                                            {data.academics && data.academics.content ? data.academics.content : ''}
-                                        </Typography>
-                                    </CardContent>
-                                </div>
-                                <Hidden xsDown>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image={data.academics && data.academics.imageUrl ? data.academics.imageUrl : ''}
-                                        title="Image Text"
-                                    />
-                                </Hidden>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Card className={classes.card}>
-                                <div className={classes.cardDetails}>
-                                    <CardContent>
-                                        <Typography component="h2" variant="h5">
-                                            {data.extracurricular && data.extracurricular.title
-                                                ? data.extracurricular.title
-                                                : ''}
-                                        </Typography>
-                                        <Typography variant="subtitle1" paragraph>
-                                            {data.extracurricular && data.extracurricular.content
-                                                ? data.extracurricular.content
-                                                : ''}
-                                        </Typography>
-                                    </CardContent>
-                                </div>
-                                <Hidden xsDown>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image={
-                                            data.extracurricular && data.extracurricular.imageUrl
-                                                ? data.extracurricular.imageUrl
-                                                : ''
-                                        }
-                                        title="Image Text"
-                                    />
-                                </Hidden>
-                            </Card>
-                        </Grid>
+                        <Grow in style={{ transformOrigin: '0 0 0' }} timeout={1500}>
+                            <Grid item xs={12} md={6}>
+                                <Card className={classes.card}>
+                                    <div className={classes.cardDetails}>
+                                        <CardContent>
+                                            <Typography component="h2" variant="h5">
+                                                {data.academics && data.academics.title ? data.academics.title : ''}
+                                            </Typography>
+                                            <Typography variant="subtitle1" paragraph>
+                                                {data.academics && data.academics.content ? data.academics.content : ''}
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <Hidden xsDown>
+                                        <CardMedia
+                                            className={classes.cardMedia}
+                                            image={
+                                                data.academics && data.academics.imageUrl ? data.academics.imageUrl : ''
+                                            }
+                                            title="Image Text"
+                                        />
+                                    </Hidden>
+                                </Card>
+                            </Grid>
+                        </Grow>
+                        <Grow in style={{ transformOrigin: '0 0 0' }} timeout={2500}>
+                            <Grid item xs={12} md={6}>
+                                <Card className={classes.card}>
+                                    <div className={classes.cardDetails}>
+                                        <CardContent>
+                                            <Typography component="h2" variant="h5">
+                                                {data.extracurricular && data.extracurricular.title
+                                                    ? data.extracurricular.title
+                                                    : ''}
+                                            </Typography>
+                                            <Typography variant="subtitle1" paragraph>
+                                                {data.extracurricular && data.extracurricular.content
+                                                    ? data.extracurricular.content
+                                                    : ''}
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                    <Hidden xsDown>
+                                        <CardMedia
+                                            className={classes.cardMedia}
+                                            image={
+                                                data.extracurricular && data.extracurricular.imageUrl
+                                                    ? data.extracurricular.imageUrl
+                                                    : ''
+                                            }
+                                            title="Image Text"
+                                        />
+                                    </Hidden>
+                                </Card>
+                            </Grid>
+                        </Grow>
                     </Grid>
                 </Container>
             </div>
