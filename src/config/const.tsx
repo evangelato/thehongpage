fbd3fe56c26ddf9c1fdc0b657c6eec82532fe93e
Evan Hong <evangelurtz@gmail.com>
2020-03-09 04:18:44 +0900

    Add production backend url

--- src/config/const.tsx
+++ src/config/const.tsx
@@ -1,1 +1,4 @@
-export const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4000/api';
+export const baseUrl =
+    process.env.NODE_ENV === 'production'
+        ? 'https://thehongpage-backend.herokuapp.com/api'
+        : 'http://localhost:4000/api';
